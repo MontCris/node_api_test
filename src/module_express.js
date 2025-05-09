e5197b9093411c9bf733ea595142bc1582403cf9
MontCris <cristianxlokl@gmail.com>
2025-05-08 22:31:41 -0400

    prueba commit

--- src/module_express.js
+++ src/module_express.js
@@ -4,3 +4,4 @@ const app = express();
 app.use(require('./routes/index')); /* import the router module from routes/index.js */
 
 module.exports = app; /* export the app  */
+console.log("Express app initialized"); /* log the initialization of the app */
